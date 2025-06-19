0bf0f805bca99fdcf043c4bd5d26bd90a3b1f4ef
DANIEL ESTEBAN JIMENEZ ANGAMARCA <danielejimeneza@gmail.com>
2025-06-18 22:44:00 -0500

    initialize archetype-karate-app with Gradle setup and basic application structure

---
+++ src/main/java/dev/daniesteb/archetypekarateapp/ArchetypeKarateAppApplication.java
@@ -0,0 +1,13 @@
+package dev.daniesteb.archetypekarateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchetypeKarateAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ArchetypeKarateAppApplication.class, args);
+  }
+
+}
